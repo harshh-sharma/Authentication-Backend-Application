f0746a74285730c4c7a57f7efeb88f0e803154e1
harshh-sharma <harsh877sharma@gmail.com>
2024-03-19 16:51:21 +0530

    some updation in clodinary config file

--- Backend/utils/cloudinary.js
+++ Backend/utils/cloudinary.js
@@ -16,6 +16,7 @@ const uploadFileOnCloudinary = async (localFilePath) => {
             }
         )
         console.log("file is uploaded on cloudinary",response.url);
+        fs.unlinkSync(localFilePath);
         return response;
     } catch (error) {  
         console.log("erro while uploading file on cloudinary",error.message);
